7bb8a3df3d8aa8d86348826c31bdc5e703ac66d9
ehamill23 <78164891+ehamill23@users.noreply.github.com>
2021-02-21 23:25:53 -0600

    Add files via upload

---
+++ FINAL3.py
@@ -0,0 +1,18 @@
+# filter primes
+# filter largest
+x = 600851475143
+primelist = []
+# prime checker 
+def factor(x):
+    for i in range(2, x - 1):
+        if x % i == 0:
+            return False
+    return True
+# calls fucntion runs through #2-x, checks if its a factor and a prime then prints i
+for i in range(2, x - 1):
+    if x % i == 0 and factor(i) == True:
+        primelist.append(i)
+
+print(max(primelist))
+            
+    
