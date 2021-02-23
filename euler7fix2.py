5717e9a5535dc49e667096c0d5548688dfff24b3
ehamill23 <78164891+ehamill23@users.noreply.github.com>
2021-02-23 01:57:04 -0600

    Add files via upload

---
+++ euler7fix2.py
@@ -0,0 +1,24 @@
+# variables
+number = 1
+counter = 0
+primelist = []
+# checks for primes
+def prime(x):
+    for number in range(2, x - 1):
+        
+        if x % number == 0:
+            return False
+    return True
+# if prime adds one, when at 10001, thats the 10001st prime
+def primecounter(x):
+    if prime(x) == True:
+        global counter
+        counter += 1
+        primelist.append(x)
+    if counter == 10002:
+            return True
+    return False
+# 
+while primecounter(number) == False:
+    number += 1
+print(max(primelist))
