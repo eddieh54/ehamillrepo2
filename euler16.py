3bf1821caab39af49ee574e7d44e68ca0e9f71bf
ehamill23 <78164891+ehamill23@users.noreply.github.com>
2021-04-23 11:16:19 -0500

    Update euler16.py

--- euler16.py
+++ euler16.py
@@ -1,10 +1,11 @@
+# variables 
 x = 2**1000
 numlist = []
-
+# makes a list of the num in x, and converts it to int
 for i in str(x):
     numlist.append(i)
     numlist = [int(i) for i in numlist]
-
+# finds the sum of the list
 print('The number is:' + str(x))
 print('The sum of its digits is:' + str(sum(numlist)))   
 
