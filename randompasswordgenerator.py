b57535dfd6156fc749fa635252645f525693e4fc
ehamill23 <78164891+ehamill23@users.noreply.github.com>
2022-02-02 13:28:35 -0600

    Add files via upload

---
+++ randompasswordgenerator.py
@@ -0,0 +1,22 @@
+import random 
+import re
+
+password = []
+ranpass = []
+
+def ran(x):
+
+  with open(x, 'r') as file:
+    alphls = []
+    for line in file:
+      file1line = line.strip()
+      alphls.append(file1line)
+    password = random.sample(alphls,5)
+    return password
+
+ranpass = ranpass + ran('alphabet.txt')
+ranpass = ranpass + ran('1-9.txt')
+ranpass = ranpass + ran('specialchar.txt')
+random.shuffle(ranpass)
+ranpass = ''.join(map(str, ranpass))
+print(ranpass)
